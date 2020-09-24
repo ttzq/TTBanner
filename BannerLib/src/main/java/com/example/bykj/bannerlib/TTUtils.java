caec0ec649aa95b22faeed83ce8a443463acb7bf
sxq <87253069@qq.com>
2020-09-24 13:45:03 +0800

    提交一下

--- BannerLib/src/main/java/com/example/bykj/bannerlib/TTUtils.java
+++ BannerLib/src/main/java/com/example/bykj/bannerlib/TTUtils.java
@@ -2,7 +2,7 @@ package com.example.bykj.bannerlib;
 
 import android.content.Context;
 import android.util.Log;
-
+//这里添加注释
 public class TTUtils {
     public  static  void  Add(Context context){
         Log.e("ss", "Add: 走进来了" );
